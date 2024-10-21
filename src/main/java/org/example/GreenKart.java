0fa65c2d35ac6a4205618bc28f5cbb874301ca1d
jcolonia <jcolonia@baufest.com>
2024-10-21 00:48:03 -0500

    uncompleted cartProducts validation

--- src/main/java/org/example/GreenKart.java
+++ src/main/java/org/example/GreenKart.java
@@ -25,26 +25,46 @@ public class GreenKart {
        String[] requestProducts = {"Cucumber", "Brocolli", "Beetroot"};
 
         driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+        List<WebElement> productsToPick = driver.findElements(By.cssSelector("h4.product-name"));
 
-        addProducts(driver, requestProducts);
+        addProducts(driver, requestProducts,productsToPick);
+
+        WebElement cartBtn = driver.findElement(By.cssSelector("img[alt='Cart']"));
+
+        cartBtn.click();
+
+        List<WebElement> cartProductNames = driver.findElements(By.cssSelector("p.product-name"));
+
+        List products = Arrays.asList(requestProducts);
+        for(int i=0; i<products.size();i++){
+
+            String product = cartProductNames.get(i).getText();
+        int count=0;
+            if(cartProductNames.contains(products.get(i))){
+                count++;
+            }
+
+            System.out.println(count +"----" + products.size());
+        }
 
 
 
+     Thread.sleep(3000);
         driver.quit();
 
 
     }
 
-    public static void addProducts(WebDriver driver, String[] requestProducts) throws InterruptedException {
+    public static void addProducts(WebDriver driver, String[] requestProducts, List<WebElement> productsToPick) throws InterruptedException {
+
+
         int j=0;
 
-        List<WebElement> productsToPick = driver.findElements(By.cssSelector("h4.product-name"));
         for(int i=0; i<productsToPick.size(); i++){
             j++;
-            String[] unformatedProduct = productsToPick.get(i).getText().split("-");
-            String productName  = unformatedProduct[0].trim();
+            List products = formatProductName(productsToPick);
             List productList = Arrays.asList(requestProducts);
-            if(productList.contains(productName)){
+            if(productList.contains(products.get(i))){
                 driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
                 Thread.sleep(2500);
 
@@ -55,5 +75,19 @@ public class GreenKart {
             }
 
         }
+
+
     }
+    public static List formatProductName (List<WebElement> productsToPick){
+        List products = new ArrayList();
+        for(int i=0; i<productsToPick.size();i++){
+            String[] unformatedProduct = productsToPick.get(i).getText().split("-");         String productName  = unformatedProduct[0].trim();
+            products.add(productName);
+        }
+
+        return products;
+    }
+
+
+
 }
