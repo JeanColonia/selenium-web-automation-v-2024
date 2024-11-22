29861144827ba0daf52b74da39bd42bfb5a550d3
jcolonia <jcolonia@baufest.com>
2024-11-21 22:52:15 -0500

    element actions

--- src/main/java/org/example/ElementInteractions.java
+++ src/main/java/org/example/ElementInteractions.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 
 public class ElementInteractions {
@@ -20,18 +21,23 @@ public class ElementInteractions {
         Actions action = new Actions(driver);
 
         WebElement accountAndList = driver.findElement(By.cssSelector("a[id='nav-link-accountList']"));
+
         action.moveToElement(accountAndList).build().perform();
 
         WebElement searchInput = driver.findElement(By.id("twotabsearchtextbox"));
 
-        action.moveToElement(searchInput).click().keyDown(Keys.SHIFT).sendKeys("hello").doubleClick().build().perform();
+        action.moveToElement(searchInput).click().keyDown(Keys.SHIFT).sendKeys("HELLO").doubleClick().build().perform();
 
-        action.moveToElement(accountAndList).contextClick().sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.RETURN).build().perform();
+        action.moveToElement(accountAndList).contextClick().keyDown(Keys.ARROW_DOWN).keyDown(Keys.ARROW_DOWN).click().build().perform();
+
+
+        Thread.sleep(2000);
 
         driver.navigate().to(driver.getCurrentUrl());
-        Thread.sleep(3500);
+
+
+        Thread.sleep(2000);
 
         driver.quit();
-
     }
 }
