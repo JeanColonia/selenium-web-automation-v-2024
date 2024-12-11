755e41a29187653f93d0a5a38012e326be8db646
Jean Colonia <engshka123@gmail.com>
2024-12-11 17:28:45 -0500

    handling http certications

---
+++ src/main/java/org/example/HandlingHTTPSCertifications.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class HandlingHTTPSCertifications {
+
+    public static void main(String []args){
+
+        //chrome options (settings)
+        ChromeOptions options = new ChromeOptions();
+
+        options.setAcceptInsecureCerts(true);
+
+        //instance of chrome driver
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://expired.badssl.com/");
+
+        System.out.println(driver.getTitle());
+
+
+
+        driver.quit();
+
+    }
+}
