f8414560ac9b293d872a08648b44d2e61a22af36
jcolonia <jcolonia@baufest.com>
2024-12-12 00:11:15 -0500

    proxies

--- src/main/java/org/example/HandlingHTTPSCertifications.java
+++ src/main/java/org/example/HandlingHTTPSCertifications.java
@@ -1,17 +1,22 @@
 package org.example;
 
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class HandlingHTTPSCertifications {
 
-    public static void main(String []args){
+    public static void main(String []args) throws InterruptedException {
 
         //chrome options (settings)
         ChromeOptions options = new ChromeOptions();
 
+        //setting proxy value
+        Proxy proxy = new Proxy();
+        //proxy.setHttpProxy("ipaddress:999");
         options.setAcceptInsecureCerts(true);
+        //options.setCapability("proxy", proxy);
 
         //instance of chrome driver
         WebDriver driver = new ChromeDriver(options);
@@ -21,7 +26,7 @@ public class HandlingHTTPSCertifications {
         System.out.println(driver.getTitle());
 
 
-
+        Thread.sleep(2500);
         driver.quit();
 
     }
