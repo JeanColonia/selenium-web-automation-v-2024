82d3e4ea39a2b181b4c5d96ddbef628cfcb0da5c
jcolonia <jcolonia@baufest.com>
2024-12-12 00:40:03 -0500

    taking a screenshot

--- src/main/java/org/example/Misscelleanous.java
+++ src/main/java/org/example/Misscelleanous.java
@@ -1,18 +1,30 @@
 package org.example;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Misscelleanous {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
 
         WebDriver driver = new ChromeDriver();
 
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
-        driver.manage().deleteCookieNamed("username");
+        //driver.manage().deleteCookieNamed("username");
+
         driver.get("http://google.com");
 
+        File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileHandler.copy(src, new File("C:\\Users\\jcolonia\\screenshot.png"));
+
+
+        driver.quit();
     }
 }
